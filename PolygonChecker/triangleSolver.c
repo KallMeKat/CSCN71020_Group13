4d032e36a67d8b0850843adbad322d8d3ee1cc96
joshua <jmshelia6910@conestogac.on.ca>
2022-11-17 19:08:34 -0500

    Put in the operation to get angles

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <math.h>
 #include "triangleSolver.h"
 
 char* analyzeTriangle(int side1, int side2, int side3) {
@@ -30,14 +30,46 @@ int isItaTriangle(int side1, int side2, int side3) {
 	{
 		printf("\n This is a Valid Tringle");
 		//then find the angles if it is a triangle (Joshua section)
+
+		// program to find the angles
+
+		// to find the largest angle
+		double cosX;
+		double x;
+
+		x = ((pow(side2, 2) + pow(side3, 2) - pow(side1, 2)) / (2 * side2 * side3));
+
+		cosX = cos (x);
+
+		printf("%lf\n", cosX);
+
+		// finding second angle
+		double cosY;
+		double y;
+
+		y = ((pow(side1, 2) + pow(side3, 2) - pow(side2, 2)) / (2 * side1 * side3));
+
+		cosY = cos(y);
+
+		printf("%lf\n", cosY);
+
+
+		// finding last angle
+		double Z;
+
+		Z = 180 - cosY - cosX;
+
+		printf("%lf\n", Z);
+
+
+
 	}
 	else
 	{
 		printf("\n This is an Invalid Triangle");
-	}
-	return 0;
-	
-	
 
+	}
+
+	return 0;
 }
 
