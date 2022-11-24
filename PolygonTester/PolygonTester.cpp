dbe78e7d02846db49f51bd85b139505faa454e52
sadiya <sislam3927@conestogac.on.ca>
2022-11-24 18:22:29 -0500

    added some test cases for triangle functions

--- PolygonTester/PolygonTester.cpp
+++ PolygonTester/PolygonTester.cpp
@@ -1,10 +1,16 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+//#include "triangleSolver.h"
+//#include "rectangleSolver.h"
+//#include "main.h"
+
 
 // Katarina and Sadiya added the test cases for the project 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 extern "C" {
 #include "../PolygonChecker/triangleSolver.h"
+#include "../PolygonChecker/rectangleSolver.h"
+#include "../PolygonChecker/main.h"
 }
 namespace PolygonTester
 {
@@ -19,4 +25,41 @@ namespace PolygonTester
 			Assert::AreEqual("Scalene triangle", actual);
 		}
 	};
+
+	TEST_CLASS(analyzingTriangle)
+	{
+	public:
+
+		TEST_METHOD(IsoscelesTriangle)
+		{
+			char* actual = analyzeTriangle(4, 2, 2);
+
+			Assert::AreEqual("Isosceles triangle", actual);
+		}
+	};
+
+	TEST_CLASS(analyzingTriangle)
+	{
+	public:
+
+		TEST_METHOD(EquilateralTriangle)
+		{
+			char* actual = analyzeTriangle(4, 4, 4);
+
+			Assert::AreEqual("Equilateral triangle", actual);
+		}
+	};
+
+	TEST_CLASS(rectanglePoints)
+	{
+	public:
+
+		TEST_METHOD(IsRectangle)
+		{
+			char* actual = analyzeRectangle(4, 3, 4, 3);
+
+			Assert::AreEqual("Is a rectangle", actual);
+		}
+	};
+
 }
