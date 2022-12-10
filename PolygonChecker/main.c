533dc6078dc35045bb5b0017f80574ad5e602401
Katarina <Klukic2950@conestogac.on.ca>
2022-12-10 12:38:16 -0500

    fixed user input verficiation, fixed the area/perimiter fo rectangle

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
 #include "main.h"
@@ -41,9 +42,12 @@ int main() {
 			break;
 		case 0:
 			continueProgram = false;
+			printf("Thank you for using the Polygon Checker!");
+			exit(EXIT_FAILURE);
 			break;
 		default:
 			printf_s("Invalid value entered.\n");
+			exit(EXIT_FAILURE);
 			break;
 		}
 	}
@@ -65,8 +69,9 @@ int printShapeMenu() {
 	printf_s("0. Exit\n");
 
 	int shapeChoice;
-	// add input validation here to be sure that if a character is entered the program won't break (Joshua)
+
 	printf_s("Enter number: ");
+	
 	scanf_s("%1o", &shapeChoice);
 
 	return shapeChoice;
@@ -74,6 +79,7 @@ int printShapeMenu() {
 
 int* getTriangleSides(int* triangleSides) {
 	printf_s("Enter the three sides of the triangle: ");
+
 	for (int i = 0; i < 3; i++)
 	{
 		scanf_s("%d", &triangleSides[i]);
@@ -88,37 +94,34 @@ RECTANGLE getRectanglePoints(void) {
 	// Katarina added the variable declarations and points scaned
 	RECTANGLE outputedrectangle;
 
+	//points using the struct POINTS
 	POINTS point1;
 	POINTS point2;
 	POINTS point3;
 	POINTS point4;
 
 	// Katarina added the points for the user to enter 
+	
 	printf("Enter the first point (x1, y1)\n");
 	scanf_s("%lf%lf", &point1.x, &point1.y);
 
-	printf("Enter the second point (x1, y1)\n");
+	printf("Enter the second point (x2, y2)\n");
 	scanf_s("%lf%lf", &point2.x, &point2.y);
 
-	printf("Enter the third point (x1, y1)\n");
+	printf("Enter the third point (x3, y3)\n");
 	scanf_s("%lf%lf", &point3.x, &point3.y);
 
-	printf("Enter the final point (x1, y1)\n");
+	printf("Enter the final point (x4, y4)\n");
 	scanf_s("%lf%lf", &point4.x, &point4.y);
 
+	//assigns the points to the struct RECTANGLE varible 
 	outputedrectangle.point1 = point1;
 	outputedrectangle.point2 = point2;
 	outputedrectangle.point3 = point3;
 	outputedrectangle.point4 = point4;
 
-
-
-
 	return outputedrectangle;
-
-	// get the sides to form the rectangle in pairs (x and y) 
-	// ex. x1 and y1, x2 and y2, x3 and y3, x4 and y4
-	// this can be done in a for loop or individually 
+	
 
 }
 
--- PolygonChecker/rectangleSolver.c
+++ PolygonChecker/rectangleSolver.c
@@ -10,24 +10,14 @@
 RECTANGLESIDES analyzeRectangle(RECTANGLE points) {
 
 	//Katarina added the distance formula to calucate the sides distance 
-	// put this in seperate function 
-//	double distance1, distance2, distance3, distance4;
-
+	
+	//varible from the struct rectside is used to assign points 
 	RECTANGLESIDES rectside;
 
 	rectside.side1=  sqrt((points.point2.x- points.point1.x) * (points.point2.x - points.point1.x) + (points.point2.y - points.point1.y) * (points.point2.y- points.point1.y));
 	rectside.side2 = sqrt((points.point3.x- points.point1.x) * (points.point3.x- points.point1.x) + (points.point3.y - points.point1.y) * (points.point3.y- points.point1.y));
 	rectside.side3 = sqrt((points.point4.x - points.point2.x) * (points.point4.x - points.point2.x) + (points.point4.y- points.point2.y) * (points.point4.y- points.point2.y));
 	rectside.side4 = sqrt((points.point4.x- points.point3.x) * (points.point4.x- points.point3.x) + (points.point4.y - points.point3.y) * (points.point4.y - points.point3.y));
-
-	printf("Distance between (%0.2lf, %0.2lf) and (%0.2lf, %0.2lf) is %0.2lf\n", points.point1.x, points.point1.y, points.point2.x, points.point2.y, rectside.side1);
-	printf("Distance between (%0.2lf, %0.2lf) and (%0.2lf, %0.2lf) is %0.2lf\n", points.point1.x, points.point1.y, points.point3.x, points.point3.y, rectside.side2);
-	printf("Distance between (%0.2lf, %0.2lf) and (%0.2lf, %0.2lf) is %0.2lf\n", points.point2.x, points.point2.y, points.point4.x, points.point4.y, rectside.side3);
-	printf("Distance between (%0.2lf, %0.2lf) and (%0.2lf, %0.2lf) is %0.2lf\n", points.point3.x, points.point3.y, points.point4.x, points.point4.y, rectside.side4);
-	// takes the pairs of points and forms 4 lines
-	// you can form lines from two points using the distance formula 
-
-	// Area of the given rectangle
 
 	return rectside;
 }
@@ -37,41 +27,81 @@ char* isItaRectangle(RECTANGLESIDES side) {
 
 	// takes those 4 lines (distance1, distance2, distance3, distance4, and compares to see if 2 of them are the same length, if they are the same length, it will report the area of the rectangle and output the perimeter 
 	char* results = "";
-	if (side.side1 == side.side2 || side.side3 == side.side4) {
+	if (side.side1 == side.side2 && side.side3 == side.side4) {
 		results = "This is a square not a rectange";
+
+		float perimeter;
+
+		perimeter = side.side1 + side.side2 + side.side3 + side.side4;
+
+		printf("The perimeter of the given shape is: %0.2f\n", perimeter);
+
+		return results;
 	}
 	else if (side.side1 == side.side2 && side.side3 == side.side4) {
 
 		results = "This is a rectangle";
+
+		float perimeter;
+
+		perimeter = side.side1 + side.side2 + side.side3 + side.side4;
+
+		printf("The perimeter of the given shape is: %0.2f\n", perimeter);
+
+
+		float area; //sadiya fixed the area function 
+
+		area = side.side1 * side.side2;
+
+		printf("The area of the given rectangle is: %0.2f\n", area);
+
+		return results;
 
 	}
 	else if (side.side1 == side.side4 && side.side3 == side.side2) {
 
 		results = "This is a rectangle";
 
+		float perimeter;
+
+		perimeter = side.side1 + side.side2 + side.side3 + side.side4;
+
+		printf("The perimeter of the given shape is: %0.2f\n", perimeter);
+
+
+		float area; //sadiya fixed the area function 
+
+		area = side.side1 * side.side2;
+
+		printf("The area of the given rectangle is: %0.2f\n", area);
+
+		return results;
+
 	}
 	else if (side.side1 == side.side3 && side.side4 == side.side2) {
 
 		results = "This is a rectangle";
 
+		float perimeter;
+
+		perimeter = side.side1 + side.side2 + side.side3 + side.side4;
+
+		printf("The perimeter of the given shape is: %0.2f\n", perimeter);
+
+
+		float area; //sadiya fixed the area function 
+
+		area = side.side1 * side.side2;
+
+		printf("The area of the given rectangle is: %0.2f\n", area);
+
+		return results;
+
 	}
 	else {
 		results = "not a rectangle or square";
+
+		return results;
 	}
-	// don't forget to add the perimeter and area of the rectangle  
-
-	float perimeter;
-
-	perimeter = side.side1 + side.side2 + side.side3 + side.side4;
-
-	printf("The perimeter of the given shape is: %0.2f\n", perimeter);
-
-
-	float area; //sadiya fixed the area function 
-
-	area = side.side1 * side.side2;
-
-	printf("The area of the given rectangle is: %0.2f\n", area);
-
-	return results;
+	
 }
